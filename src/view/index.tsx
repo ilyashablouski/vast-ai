6fd5b78b8ac3840447db064c8d4518aea17b0ec6
i.shablovskiy <ilya.shablovskiy@gmail.com>
2024-10-31 02:37:07 +0300

    remove react fragment

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -8,29 +8,27 @@ import { Logo as LogoIcon } from '../components/icons';
 
 const BaseView = () => {
   return (
-    <>
-      <AppBar position="static">
-        <Container maxWidth="lg">
-          <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
-            <Stack direction="row" alignItems="center">
-              <LogoIcon />
+    <AppBar position="static">
+      <Container maxWidth="lg">
+        <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
+          <Stack direction="row" alignItems="center">
+            <LogoIcon />
 
-              <Typography
-                ml={1}
-                component="span"
-                fontFamily='"Source Code Pro", monospace;'
-                fontWeight="600"
-                fontSize="16px"
-              >
-                Dataxide AI
-              </Typography>
-            </Stack>
+            <Typography
+              ml={1}
+              component="span"
+              fontFamily='"Source Code Pro", monospace;'
+              fontWeight="600"
+              fontSize="16px"
+            >
+              Dataxide AI
+            </Typography>
+          </Stack>
 
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 
