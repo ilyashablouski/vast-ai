81ded02ef07d1924c8b2d0f00adba3261130e16f
i.shablovskiy <ilya.shablovskiy@gmail.com>
2024-10-31 02:32:45 +0300

    add raw header

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,10 +1,36 @@
-import { Container } from '@mui/material';
+import { Container, Stack } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+import { Logo as LogoIcon } from '../components/icons';
 
 const BaseView = () => {
   return (
-    <Container maxWidth="sm">
-      <header></header>
-    </Container>
+    <>
+      <AppBar position="static">
+        <Container maxWidth="lg">
+          <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
+            <Stack direction="row" alignItems="center">
+              <LogoIcon />
+
+              <Typography
+                ml={1}
+                component="span"
+                fontFamily='"Source Code Pro", monospace;'
+                fontWeight="600"
+                fontSize="16px"
+              >
+                Dataxide AI
+              </Typography>
+            </Stack>
+
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
   );
 };
 
