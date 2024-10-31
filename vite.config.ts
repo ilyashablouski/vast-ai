3722cb8f15c7a42e7551fda9b1166fd0b96bce45
i.shablovskiy <ilya.shablovskiy@gmail.com>
2024-10-31 17:54:21 +0300

    add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@assets': resolve(__dirname, './src/assets'),
+      '@components': resolve(__dirname, './src/components'),
+    },
+  },
   plugins: [react()],
-})
+});
