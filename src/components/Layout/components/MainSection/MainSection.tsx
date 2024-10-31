ddd0643870cb441ff6e3ec588a6fceca4ed6eb7f
i.shablovskiy <ilya.shablovskiy@gmail.com>
2024-11-01 01:30:24 +0300

    add card to main section

--- src/components/Layout/components/MainSection/MainSection.tsx
+++ src/components/Layout/components/MainSection/MainSection.tsx
@@ -1,5 +1,55 @@
+import { Card, Container, Stack } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+import { configs } from '@/mock/configurations.ts';
+
 const MainSection = () => {
-  return <div>MainSection</div>;
+  return (
+    <Container sx={{ mt: 6 }}>
+      <Typography variant="h4">Available Configurations</Typography>
+
+      <Stack spacing={3}>
+        {configs.map((config) => (
+          <Card sx={{ p: 3, minHeight: '111px' }}>
+            <Stack direction="row" spacing={7} justifyContent="space-between">
+              <Stack>
+                <Typography mb={2}>{config.general.name}</Typography>
+                <Typography mt="auto">{config.general.place}</Typography>
+              </Stack>
+
+              <Stack whiteSpace="pre">
+                <Typography mb={1}>CPU:</Typography>
+                <Typography mt="auto">{config.cpu.model}</Typography>
+              </Stack>
+
+              <Stack whiteSpace="pre">
+                <Typography mb={1}>Storage:</Typography>
+                <Typography mt="auto">{config.storage.model}</Typography>
+              </Stack>
+
+              <Stack whiteSpace="pre">
+                <Typography mb={1}>Connection:</Typography>
+                <Stack mt="auto" spacing="4px">
+                  <Typography>{config.connection.upload}</Typography>
+                  <Typography>{config.connection.download}</Typography>
+                </Stack>
+              </Stack>
+
+              <Stack width="160px">
+                <Typography mb={1.5} textAlign="right">
+                  $&nbsp;{config.rent}/hr
+                </Typography>
+                <Button variant="contained" size="small" sx={{ mt: 'auto' }}>
+                  Rent
+                </Button>
+              </Stack>
+            </Stack>
+          </Card>
+        ))}
+      </Stack>
+    </Container>
+  );
 };
 
 export default MainSection;
---
+++ src/mock/configurations.ts
@@ -0,0 +1,19 @@
+export const configs = [
+  {
+    general: {
+      name: '2x H100 SXM',
+      place: 'Thailand, TH',
+    },
+    cpu: {
+      model: 'AMD EPYC 9124\n' + '32.0/64 cpu' + '257/516 GB',
+    },
+    storage: {
+      model: 'nvme\n' + '2429 MB/s' + '37.0 GB',
+    },
+    connection: {
+      upload: '4292',
+      download: '7592',
+    },
+    rent: '4.271',
+  },
+];
