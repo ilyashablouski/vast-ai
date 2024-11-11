50cb5c32a40496f87af7310fc21154460ac1a597
i.shablovskiy <ilya.shablovskiy@gmail.com>
2024-11-11 16:12:19 +0300

    fix arrows icons

--- src/components/icons/arrow.down.icon.tsx
+++ src/components/icons/arrow.down.icon.tsx
@@ -2,7 +2,12 @@ import { createSvgIcon } from '@mui/material';
 
 export const ArrowDown = createSvgIcon(
   <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M8 2L8 14M8 14L10 12M8 14L6 12" stroke="currentColor" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M7.99999 14.7071L5.64644 12.3536L6.35355 11.6464L7.49999 12.7929L7.49999 2L8.49999 2L8.49999 12.7929L9.64644 11.6464L10.3535 12.3536L7.99999 14.7071Z"
+      fill="currentColor"
+    />
   </svg>,
   'ArrowDown',
 );
--- src/components/icons/arrow.up.icon.tsx
+++ src/components/icons/arrow.up.icon.tsx
@@ -2,7 +2,12 @@ import { createSvgIcon } from '@mui/material';
 
 export const ArrowUp = createSvgIcon(
   <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M8 14V2M8 2L6 4M8 2L10 4" stroke="currentColor" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8.00001 1.29289L10.3536 3.64645L9.64645 4.35355L8.50001 3.20711V14H7.50001V3.20711L6.35356 4.35355L5.64645 3.64645L8.00001 1.29289Z"
+      fill="currentColor"
+    />
   </svg>,
   'ArrowUp',
 );
