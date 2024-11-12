d0963e3152c9331f7c3770bcca1eb3221dc4cef2
i.shablovskiy <ilya.shablovskiy@gmail.com>
2024-11-12 15:33:45 +0300

    add alert error icon

---
+++ src/components/icons/error.icon.tsx
@@ -0,0 +1,24 @@
+import { createSvgIcon } from '@mui/material';
+
+export const Error = createSvgIcon(
+  <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M2.20164 18.4695L10.1643 4.00506C10.9021 2.66498 13.0979 2.66498 13.8357 4.00506L21.7984 18.4695C22.4443 19.6428 21.4598 21 19.9627 21H4.0373C2.54022 21 1.55571 19.6428 2.20164 18.4695Z"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path d="M12 9V13" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      d="M12 17.0195V17"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>,
+  'Error',
+);
+
+export default Error;
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,3 +3,4 @@ export * from './cross.icon';
 export * from './arrow.up.icon';
 export * from './arrow.down.icon';
 export * from './geo.icon';
+export * from './error.icon';
